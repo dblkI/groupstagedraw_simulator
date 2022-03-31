017cdceafd1f22c48d6a1cb71667cd492c617d70
Daniel Blanquel <dblanquelg1500@alumno.ipn.mx>
2022-03-31 14:29:51 -0600

    Pots added

---
+++ sorteo2022.py
@@ -0,0 +1,56 @@
+### PROGRAMA QUE CORRE EL SORTEO PARA LA FASE DE GRUPOS DEL MUNDIAL QATAR 2022
+#
+# Author: dblkl_
+
+import random
+
+def run():
+    
+    Pot1 = {
+        'Qatar'
+        'Brazil',
+        'Belgium',
+        'France',
+        'Argentina',
+        'England',
+        'Spain',
+        'Portugal'
+    }
+
+    Pot2 = {
+        'Mexico',
+        'Netherlands',
+        'Denmark',
+        'Germany',
+        'Uruguay',
+        'Switzerland',
+        'USA',
+        'Croatia'
+    }
+
+    Pot3 = {
+        'Senegal',
+        'IR Iran',
+        'Japan',
+        'Morocco',
+        'Serbia',
+        'Poland',
+        'Korea Republic',
+        'Tunisia'
+    }
+
+    Pot4 = {
+        'Cameroon',
+        'Canada',
+        'Ecuador',
+        'Saudi Arabia',
+        'Ghana',
+        'CONMEBOL/AFC',
+        'CONCACAF/OFC',
+        'UEFA'
+    }
+
+    
+
+if __name__ == '__main__':
+    run()
